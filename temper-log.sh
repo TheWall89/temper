2fa7c7cc77da508dc61c07f9f660e14a88f3738c
Matteo Pergolesi <matpergo@gmail.com>
2020-06-15 20:28:46 +0200

    Fix script with cd and local paths.

--- temper-log.sh
+++ temper-log.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-FILE="$1"
+cd /root/temper
+
 DATE=$(date --iso-8601='minute')
 INDOOR=$(./temper.py -i -o -8)
 OUTDOOR="NA" # TODO
-if [ "$(wc -l "${FILE}" | cut -d' ' -f1)" -gt 100 ]; then
-   sed -i -e '2d' "${FILE}"
+if [ "$(wc -l ./temper-log.csv | cut -d' ' -f1)" -gt 100 ]; then
+   sed -i -e '2d' ./temper-log.csv
 fi
-echo "$DATE,$INDOOR,$OUTDOOR" >> "${FILE}"
+echo "$DATE,$INDOOR,$OUTDOOR" >> ./temper-log.csv
