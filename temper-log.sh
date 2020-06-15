2895c0b646c11df5d06dc0415e8563872eb04fb1
Matteo Pergolesi <matpergo@gmail.com>
2020-06-15 20:31:45 +0200

    Long arguments for better readability.

--- temper-log.sh
+++ temper-log.sh
@@ -3,7 +3,7 @@
 cd /root/temper
 
 DATE=$(date --iso-8601='minute')
-INDOOR=$(./temper.py -i -o -8)
+INDOOR=$(./temper.py --internal --offset -8)
 OUTDOOR="NA" # TODO
 if [ "$(wc -l ./temper-log.csv | cut -d' ' -f1)" -gt 100 ]; then
    sed -i -e '2d' ./temper-log.csv
